ad76e1c02e00ef339a3c0de6218ee4a6484d1f4f
crius_keer <973968629@qq.com>
2020-07-17 11:48:46 +0800

    init

---
+++ lib/app.dart
@@ -0,0 +1,69 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart' hide Action;
+
+/// create by crius on 2020/7/17
+/// email:criusker@163.com
+class AppRoute{
+  static AbstractRoutes _global;
+  static AbstractRoutes get global{
+    if (_global == null) {
+      _global = PageRoutes(
+          pages:<String, Page<Object, dynamic>>{
+
+          },
+
+          visitor: (String path, Page<Object, dynamic>page){
+            ///Aop
+            /// 页面可以有一些私有的 AOP 的增强， 但往往会有一些 AOP 是整个应用下，所有页面都会有的。
+            /// 这些公共的通用 AOP ，通过遍历路由页面的形式统一加入。
+            page.enhancer.append(
+
+              ///[viewMiddleware] 视图中间件 View Aop
+              viewMiddleware: <ViewMiddleware<dynamic>>[
+                safetyView<dynamic>(),
+              ],
+
+              /// Adapter AOP 适配器切面
+              adapterMiddleware: <AdapterMiddleware<dynamic>>[
+                safetyAdapter<dynamic>()
+              ],
+
+              effectMiddleware: <EffectMiddleware<dynamic>>[
+                _pageAnalyticsMiddleware<dynamic>(),
+              ],
+
+              /// Store AOP
+              middleware: <Middleware<dynamic>>[
+                logMiddleware<dynamic>(tag: page.runtimeType.toString()),
+              ],
+
+            );
+          }
+      );
+    }
+    return _global;
+  }
+}
+
+Widget createApp(){
+  return MaterialApp(
+    title: "KitDemo",
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: Center(child: Text("KitDemo"))),
+  );
+}
+
+/// 简单的 Effect AOP
+/// 只针对页面的生命周期进行打印
+EffectMiddleware<T> _pageAnalyticsMiddleware<T>({String tag = 'redux'}) {
+  return (AbstractLogic<dynamic> logic, Store<T> store) {
+    return (Effect<dynamic> effect) {
+      return (Action action, Context<dynamic> ctx) {
+        if (logic is Page<dynamic, dynamic> && action.type is Lifecycle) {
+          print('${logic.runtimeType} ${action.type.toString()} ');
+        }
+        return effect?.call(action, ctx);
+      };
+    };
+  };
+}
